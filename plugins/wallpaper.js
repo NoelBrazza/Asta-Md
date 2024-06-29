bf7df26c4aa0bdb5050bffb519dc596354a60c1f
NoelBrazza <160472950+NoelBrazza@users.noreply.github.com>
2024-06-29 13:30:37 +0300

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -84,63 +84,7 @@ smd(
       }
     }
   );
-  // Command 3: cat
-smd(
-    {
-      pattern: "cat",
-      category: "wallpaper",
-      filename: __filename,
-      desc: "Get a cat wallpaper.",
-    },
-    async (m) => {
-      try {
-        let apiUrl = "https://api.maher-zubair.tech/wallpaper/cat";
-        let response = await fetch(apiUrl);
-        let jsonResponse = await response.json();
-  
-        if (jsonResponse.status === 200) {
-          await m.send(jsonResponse.url, { caption: Config.caption }, "image", m);
-        } else {
-          await m.send("*_Request not be preceed!!_*");
-        }
-      } catch (error) {
-        await m.error(
-          error + "\n\ncommand: cat",
-          error,
-          "*_No responce from API, Sorry!!_*"
-        );
-      }
-    }
-  );
-  // Command 4: dog
-smd(
-    {
-      pattern: "dog",
-      category: "wallpaper",
-      filename: __filename,
-      desc: "Get a dog wallpaper.",
-    },
-    async (m) => {
-      try {
-        let apiUrl = "https://api.maher-zubair.tech/wallpaper/dog";
-        let response = await fetch(apiUrl);
-        let jsonResponse = await response.json();
-  
-        if (jsonResponse.status === 200) {
-          await m.send(jsonResponse.url, { caption: Config.caption }, "image", m);
-        } else {
-          await m.send("*_Request not be preceed!!_*");
-        }
-      } catch (error) {
-        await m.error(
-          error + "\n\ncommand: dog",
-          error,
-          "*_No responce from API, Sorry!!_*"
-        );
-      }
-    }
-  );
-  // Command 5: messi
+  // Command 3: messi
 smd(
     {
       pattern: "messi",
@@ -168,7 +112,7 @@ smd(
       }
     }
   );
-  // Command 6: mlegend
+  // Command 4: mlegend
 smd(
     {
       pattern: "mlegend",
@@ -196,7 +140,7 @@ smd(
       }
     }
   );
-  // Command 7: pubg
+  // Command 5: pubg
 smd(
     {
       pattern: "pubg",
@@ -225,7 +169,7 @@ smd(
     }
   );
   
-  // Command 8: random
+  // Command 6: random
   smd(
     {
       pattern: "random",
@@ -254,7 +198,7 @@ smd(
     }
   );
   
-  // Command 9: car
+  // Command 7: car
   smd(
     {
       pattern: "car",
@@ -283,7 +227,7 @@ smd(
     }
   );
   
-  // Command 10: blackpink
+  // Command 8: blackpink
   smd(
     {
       pattern: "blackpink",
